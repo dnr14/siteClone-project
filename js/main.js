5ff0d1d3a134703491f3469c6af99076b1c8b947
dnr14 <dnr14@naver.com>
2021-06-15 21:35:04 +0900

    반응형 슬라이드 완성

--- js/main.js
+++ js/main.js
@@ -2,35 +2,46 @@ class Slider {
   constructor() {
     this.$as = Array.from([...document.querySelectorAll('.slider>a')]);
     this.$spanIcon = document.querySelector('span.icon');
+    this.$sliderBox = document.querySelector('.slider_bar .bar');
+    this.$drag = document.querySelector('.slider_bar .bar .drag');
+
+
     this.$aWidth = this.$as[0].offsetWidth;
-
-
-    this.index = 0;
+    this.currentTransformX = 0;
     this.innerWidth = window.innerWidth;
 
     this.fullWidth = this.$as.reduce((l, r) => l + r.offsetWidth, 0);
-
-    this.$sliderBox = document.querySelector('.slider_bar .bar');
-    this.$drag = document.querySelector('.slider_bar .bar .drag');
     this.$drag.style.width = `${(this.$sliderBox.offsetWidth / this.fullWidth) * this.$aWidth}px`;
-    this._1퍼센드 = (this.fullWidth - this.$aWidth) / (this.$sliderBox.offsetWidth - this.$drag.offsetWidth);
 
     this.init();
   }
 
+  set_1퍼센트() {
+    if (this.innerWidth < 600) {
+      this._1퍼센드 = (this.fullWidth - this.$aWidth) / (this.$sliderBox.offsetWidth - this.$drag.offsetWidth);
+    } else if (this.innerWidth < 900) {
+      this._1퍼센드 = (this.fullWidth - (this.$aWidth * 2)) / (this.$sliderBox.offsetWidth - this.$drag.offsetWidth);
+    } else if (this.innerWidth > 900) {
+      this._1퍼센드 = (this.fullWidth - (this.$aWidth * 3)) / (this.$sliderBox.offsetWidth - this.$drag.offsetWidth);
+    }
+  }
 
   init() {
     this.eventHanlder();
+    this.set_1퍼센트();
   }
 
   event = () => {
-    this.$drag.style.width = `${(this.$sliderBox.offsetWidth / this.fullWidth) * this.$aWidth}px`;
 
+    this.$aWidth = this.$as[0].offsetWidth;
+    this.currentTransformX = 0;
     this.innerWidth = window.innerWidth;
-    this.$as.forEach(a => {
-      a.removeAttribute('style');
-    });
-    this.index = 0;
+    this.fullWidth = this.$as.reduce((l, r) => l + r.offsetWidth, 0);
+    this.$drag.style.width = `${(this.$sliderBox.offsetWidth / this.fullWidth) * this.$aWidth}px`;
+    this.set_1퍼센트();
+
+    this.$as.forEach(a => a.removeAttribute('style'));
+    this.$drag.style.left = '0px';
   }
 
 
@@ -47,53 +58,67 @@ class Slider {
 
   sliderBarMove(x) {
     const result = Math.round(this._1퍼센드 * x);
+    this.currentTransformX = result;
     this.$as.forEach(a => {
       a.removeAttribute('style');
       a.style.transform = `translateX(-${result}px)`;
     });
   }
 
+  sliderMove = () => {
+    const $a = this.$as[0];
+
+    if (this.innerWidth < 600) {
+      const fullWidth = $a.offsetWidth * 4;
+      if ($a.style.transform === `translateX(-${fullWidth}px)`) return;
+
+      const { offsetWidth } = $a;
+      this.currentTransformX += offsetWidth;
+
+      if (this.currentTransformX > fullWidth) this.currentTransformX = fullWidth;
+
+      // this.$as.forEach(a => a.style.transform = `translateX(-${this.currentTransformX}px)`);
+      // this.$drag.style.left = `${Math.round(this.currentTransformX / this._1퍼센드)}px`;
+    }
+    else if (this.innerWidth < 900) {
+
+      const fullWidth = $a.offsetWidth * 3;
+      if ($a.style.transform === `translateX(-${fullWidth}px)`) return;
+
+      const { offsetWidth } = $a;
+
+      $a.style.transform === `translateX(-${offsetWidth * 2}px)`
+        ? this.currentTransformX += offsetWidth
+        : this.currentTransformX += offsetWidth * 2;
+
+      if (this.currentTransformX > fullWidth) this.currentTransformX = fullWidth;
+
+      // this.$as.forEach(a => a.style.transform = `translateX(-${this.currentTransformX}px)`);
+      // this.$drag.style.left = `${Math.round(this.currentTransformX / this._1퍼센드)}px`;
+    }
+    else if (this.innerWidth >= 900) {
+      const fullWidth = $a.offsetWidth * 2;
+      if ($a.style.transform === `translateX(-${fullWidth}px)`) return;
+
+      const { offsetWidth } = $a;
+      this.currentTransformX += offsetWidth * 2;
+
+      if (this.currentTransformX > fullWidth) this.currentTransformX = fullWidth;
+
+    }
+    this.$as.forEach(a => a.style.transform = `translateX(-${this.currentTransformX}px)`);
+    this.$drag.style.left = `${Math.round(this.currentTransformX / this._1퍼센드)}px`;
+  }
+
   eventHanlder() {
     window.addEventListener('resize', this.event);
     this.$drag.addEventListener('mousedown', this.dragMouseDown);
     this.$sliderBox.addEventListener('click', this.sliderBarClick);
-    this.$spanIcon.addEventListener('click', () => {
-
-      if (this.innerWidth < 600) {
-        if (this.$as[0].style.transform === `translateX(-${this.$as[0].offsetWidth * 4}px)`) return;
-        const { offsetWidth } = this.$as[0];
-        this.index += offsetWidth;
-        this.$as.forEach(a => {
-          a.style.transform = `translateX(-${this.index}px)`;
-        });
-      } else if (this.innerWidth < 900) {
-        if (this.$as[0].style.transform === `translateX(-${this.$as[0].offsetWidth * 3}px)`) return;
-
-        const { offsetWidth } = this.$as[0];
-
-        this.$as[0].style.transform === `translateX(-${this.$as[0].offsetWidth * 2}px)` ?
-          this.index += offsetWidth : this.index += offsetWidth * 2;
-
-        this.$as.forEach(a => {
-          a.style.transform = `translateX(-${this.index}px)`;
-        });
-
-      } else if (this.innerWidth >= 900) {
-        if (this.$as[0].style.transform === `translateX(-${this.$as[0].offsetWidth * 2}px)`) return;
-
-        const { offsetWidth } = this.$as[0];
-        this.index += offsetWidth * 2;
-
-        this.$as.forEach(a => {
-          a.style.transform = `translateX(-${this.index}px)`;
-        });
-      }
-
-    });
+    this.$spanIcon.addEventListener('click', this.sliderMove);
   }
 
   dragMouseDown = (e) => {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let pos1 = 0, pos3 = 0, pos4 = 0;
 
     e = e || window.event;
     e.preventDefault();
@@ -104,16 +129,14 @@ class Slider {
       e = e || window.event;
       e.preventDefault();
       pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
       pos3 = e.clientX;
       pos4 = e.clientY;
 
-      // if (this.$drag.offsetTop - pos2 >= 0 && this.$drag.offsetTop - pos2 <= (this.$sliderBox.offsetHeight - this.$drag.clientHeight)) {
-      //   this.$drag.style.top = (this.$drag.offsetTop - pos2) + "px";
-      // }
       if (this.$drag.offsetLeft - pos1 >= 0 && this.$drag.offsetLeft - pos1 <= (this.$sliderBox.offsetWidth - this.$drag.clientWidth)) {
         this.$drag.style.left = (this.$drag.offsetLeft - pos1) + "px";
       }
+
+      // this.set_1퍼센트();
       this.sliderBarMove(this.$drag.offsetLeft);
     }
 
@@ -124,9 +147,7 @@ class Slider {
 
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
-
   }
-
 }
 
 
